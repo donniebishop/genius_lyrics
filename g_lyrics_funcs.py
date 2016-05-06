76acac0bcbe60f8da827aa2b343736bab99d8b9d
Donnie Bishop <donnie.a.bishop@gmail.com>
2016-05-06 10:10:38 -0400

    Annotation class and function

--- g_lyrics_funcs.py
+++ g_lyrics_funcs.py
@@ -10,11 +10,13 @@ API = 'https://api.genius.com'
 ACCESS_TOKEN = 'Za979SFA7_pwCdjDaAsBhnPb3A5jSgcCMixyiDeJv7U415u3ko6Qd14HIJzqrXFj'
 
 class HitResult():
-    def __init__(self, artist, title, song_id, url):
+    ''' Class for representing metadata of search results. '''
+    def __init__(self, artist, title, song_id, url, api_call):
         self.artist = artist
         self.title = title
         self.song_id = song_id
         self.url = url
+        self.api_call = api_call
 
     def form_output(self):
         '''Forms lyric sheet output for either paging or printing directly to a terminal.'''
@@ -38,16 +40,17 @@ def genius_search(query):
     search_request_object = requests.get(search_endpoint, params=payload)
 
     if search_request_object.status_code == 200:
-        json_response = search_request_object.json()
+        s_json_response = search_request_object.json()
+        api_call = search_request_object.url
 
         # Get search entry information from JSON response
-        for hit in json_response['response']['hits']:
+        for hit in s_json_response['response']['hits']:
             artist = hit['result']['primary_artist']['name']
             title = hit['result']['title']
             song_id = hit['result']['id']
             url = hit['result']['url']
 
-            results.append(HitResult(artist, title, song_id, url))
+            results.append(HitResult(artist, title, song_id, url, api_call))
 
     elif 400 <= search_request_object.status_code < 500:
         six.print_('[!] Uh-oh, something seems wrong...')
@@ -96,12 +99,13 @@ def pick_from_search(results_array):
 # Referents section
 
 class Referent():
-    """docstring for Referent"""
-    def __init__(self, classification, fragment, ref_id, url):
+    """ Class for representing referents and their respective annotation. """
+    def __init__(self, classification, fragment, annotation_id, url, api_call):
         self.classification = classification
         self.fragment = fragment
-        self.ref_id = ref_id
+        self.annotation_id = annotation_id
         self.url = url
+        self.api_call = api_call
 
 
 def get_referents(song_id):
@@ -114,16 +118,50 @@ def get_referents(song_id):
     referents_request_object = requests.get(referents_endpoint, params=payload)
 
     if referents_request_object.status_code == 200:
-        json_response = referents_request_object.json()
+        r_json_response = referents_request_object.json()
+        api_call = referents_request_object.url
 
-        for r in json_response['response']['referents']:
+        for r in r_json_response['response']['referents']:
             c = r['classification']
             f = r['fragment']
-            r_id = r['id']
+            a_id = r['id']
             r_url = r['url']
 
-            referents.append(Referent(c, f, r_id, r_url))
+            referents.append(Referent(c, f, a_id, r_url, api_call))
     elif referents_request_object.status_code >= 500:
         pass
 
     return referents
+
+# Annotations section
+
+class Annotation():
+    """ Class for reprsentation of annotation metadata. """
+    def __init__(self, text, share_url, url, votes, api_call):
+        self.text = text
+        self.share_url = share_url
+        self.url = url
+        self.votes = votes
+        self.api_call = api_call
+
+def get_annotation(annotation_id):
+    '''' Used to pull specific annotation for lyrics. '''
+
+    annotations_endpoint = API + '/annotations/' + str(annotation_id)
+    payload = {'access_token': ACCESS_TOKEN}
+    annotations_request_object = requests.get(annotations_endpoint, params=payload)
+
+    if annotations_request_object.status_code == 200:
+        api_call = annotations_request_object.url
+        a_json_response = annotations_request_object.json()
+        a = a_json_response['response']['annotation']
+
+        text = a['body']['dom']['children'][0]['children'][0]
+        share_url = a['share_url']
+        url = a['url']
+        votes = a['votes_total']
+
+    elif annotations_request_object.status_code >= 500:
+        pass
+
+    return Annotation(text, share_url, url, votes, api_call)
