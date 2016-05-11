851af2074dcc844f6536d5e79476c2d1038a94f7
Donnie Bishop <donnie.a.bishop@gmail.com>
2016-05-11 15:47:41 -0400

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='genius_lyrics',
+    version='v0.2',
+    description='The Python genius.com command line lyric searcher.',
+    url='https://github.com/donniebishop/genius_lyrics',
+    author='Donnie Bishop',
+    author_email='donnie.a.bishop@gmail.com',
+    license='MIT',
+    packages=find_packages(),
+)
